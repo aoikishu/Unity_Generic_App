e9186d68fd59e41eca69672cddc4ff75c917d42b
Keith Marshall <kam0018@gmail.com>
2019-10-01 18:01:20 -0500

    Initial commit. Menu buttons WIP.

---
+++ Assets/Scripts/PanelCreator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelCreator : MonoBehaviour
+{
+    public GameObject templatePanelPrefab;
+    private GameObject panel;
+
+    public void TogglePanel(bool value)
+    {
+        panel.SetActive(value);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel = Instantiate(templatePanelPrefab, transform, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
